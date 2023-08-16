d50d14ebd7d1747ea0c90ef5f14d8661ed2fa4bf
kevin1010607 <kevin.1010607@gmail.com>
2023-08-16 11:49:10 +0800

    fix bug in app.py

--- service/app.py
+++ service/app.py
@@ -10,9 +10,9 @@ app = Flask(__name__)
 @app.route('/')
 def root():
     name = os.environ.get('SERVICE_NAME', 'Unknown')
-    exec_time = int(os.environ.get('EXEC_TIME', 5))
+    exec_time = float(os.environ.get('EXEC_TIME', 0.5))
     time.sleep(exec_time)
-    return f'{random.random()}\n'
+    return f'{random.random():.5f}\n'
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
